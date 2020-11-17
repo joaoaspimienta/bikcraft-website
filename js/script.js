86fa7a1051cc0fe16854af111277c43bdb325c45
Joao Alejandro <joaoalejandro32@gmail.com>
2020-11-17 00:32:18 -0300

    Js Slide Portfolio Incompleto

--- js/script.js
+++ js/script.js
@@ -3,3 +3,9 @@ new SimpleSlide({
     slide: "quote", // nome do atributo data-slide="principal"
     time: 5000, // tempo de transição dos slides
 });
+
+new SimpleSlide({
+    slide: "portfolio", // nome do atributo data-slide="principal"
+    time: 5000, // tempo de transição dos slides
+    nav: true,
+});
